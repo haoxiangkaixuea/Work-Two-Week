56316fefb0f75ec5cd5d8e16de067a681349a627
Roger <63828530+haoxiangkaixuea@users.noreply.github.com>
2020-07-28 10:23:46 +0800

    初次提交

---
+++ app/src/main/java/cn/edu/scujcc/worktwoweek/AnotherRight.java
@@ -0,0 +1,18 @@
+package cn.edu.scujcc.worktwoweek;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class AnotherRight extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_another_right, container, false);
+        return view;
+    }
+}
