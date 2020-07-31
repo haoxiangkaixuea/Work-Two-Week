2220f17e36363996ebc6fcbabce3e88d19a90fe8
Roger <63828530+haoxiangkaixuea@users.noreply.github.com>
2020-07-31 17:23:49 +0800

    补交十二

--- app/src/main/java/cn/edu/scujcc/worktwoweek/MainActivity.java
+++ app/src/main/java/cn/edu/scujcc/worktwoweek/MainActivity.java
@@ -57,8 +57,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         replaceFragment(new RightFragment());
 
         //Service
-        mStart = findViewById(R.id.startservice);
-        mStop = findViewById(R.id.stopservice);
+        mStart = findViewById(R.id.startserivce);
+        mStop = findViewById(R.id.stopserivce);
         mStart.setOnClickListener(this);
         mStop.setOnClickListener(this);
 
@@ -147,12 +147,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             //启动服务
-            case R.id.startservice:
+            case R.id.startserivce:
                 Intent startIntent = new Intent(this, MyService.class);
                 startService(startIntent);
                 break;
             //停止服务
-            case R.id.stopservice:
+            case R.id.stopserivce:
                 Intent stopIntent = new Intent(this, MyService.class);
                 stopService(stopIntent);
                 break;
@@ -176,76 +176,118 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             //notification
             case R.id.send_notice:
-                // 通知渠道组的id.
-                String group = "my_group_01";
-                // 用户可见的通知渠道组名称.
-                CharSequence name = getString(R.string.group_name);
-                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                mNotificationManager.createNotificationChannelGroup(new NotificationChannelGroup(group, name));
-                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                // 通知渠道的id
-                String id = "my_channel_01";
-                // 用户可以看到的通知渠道的名字.
-                CharSequence name1 = getString(R.string.channel_name);
-                // 用户可以看到的通知渠道的描述
-                String description = getString(R.string.channel_description);
-                int importance = NotificationManager.IMPORTANCE_HIGH;
-                NotificationChannel mChannel = new NotificationChannel(id, name1, importance);
-                // 配置通知渠道的属性
-                mChannel.setDescription(description);
-                // 设置通知出现时的闪灯（如果 android 设备支持的话）
-                mChannel.enableLights(true);
-                mChannel.setLightColor(Color.RED);
-                // 设置通知出现时的震动（如果 android 设备支持的话）
-                mChannel.enableVibration(true);
-                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-                //最后在notificationManager中创建该通知渠道
-                mNotificationManager.createNotificationChannel(mChannel);
-                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                // 为该通知设置一个id
-                int notifyId = 1;
-                // 通知渠道的id
-                String chandalId = "my_channel_01";
-                // Create a notification and set the notification channel.
-                // 发布通知
-                //实现通知的点击效果，使用PendingIntent来启动一个通知活动
-                Intent intentNotice = new Intent(this, NotificationActivity.class);
-                PendingIntent pi = PendingIntent.getActivity(this, 0, intentNotice, 0);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // 通知渠道组的id.
+                    String group = "my_group_01";
+                    // 用户可见的通知渠道组名称.
+                    CharSequence name = getString(R.string.group_name);
+                    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationManager.createNotificationChannelGroup(new NotificationChannelGroup(group, name));
+                    mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    // 通知渠道的id
+                    String id = "my_channel_01";
+                    // 用户可以看到的通知渠道的名字.
+                    CharSequence name1 = getString(R.string.channel_name);
+                    // 用户可以看到的通知渠道的描述
+                    String description = getString(R.string.channel_description);
+                    int importance = NotificationManager.IMPORTANCE_HIGH;
+                    NotificationChannel mChannel = new NotificationChannel(id, name1, importance);
+                    // 配置通知渠道的属性
+                    mChannel.setDescription(description);
+                    // 设置通知出现时的闪灯（如果 android 设备支持的话）
+                    mChannel.enableLights(true);
+                    mChannel.setLightColor(Color.RED);
+                    // 设置通知出现时的震动（如果 android 设备支持的话）
+                    mChannel.enableVibration(true);
+                    mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                    //最后在notificationManager中创建该通知渠道
+                    mNotificationManager.createNotificationChannel(mChannel);
+                    mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    // 为该通知设置一个id
+                    int notifyId = 1;
+                    // 通知渠道的id
+                    String chandalId = "my_channel_01";
 
-                //创建通知首先要创建一个NotificationManager来对通知进行管理，通过getSystemService获取到
-                //里面需要穿一个字符串，一般传Context.NOTIFICATION_SERVICE
-                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                //接下来要用Builder来构造Notification对象，这里我们使用NotificationCompat类来构造创建Notification对象
-                //确保我们的程序字啊所有android系统版本都能运行
-                Notification notification = new NotificationCompat.Builder(this, "default")
-                        //通知的标题
-                        .setContentTitle("This is content Title")
-                        //通知的内容
-                        .setStyle(new NotificationCompat.BigTextStyle().bigText("method int androidx.appcompat.widget." +
-                                "DropDownListView.lookForSelectablePosition(int, boolean) \" +\n" +
-                                "  \"would have incorrectly overridden the package-private " +
-                                "method in android.widget.ListView<"))
-                        //显示大图
-                        // .setStyle(new NotificationCompat.BigPictureStyle().bigPicture
-                        //(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)))
-                        //发出通知的时间
-                        .setWhen(System.currentTimeMillis())
-                        //通知的小标题
-                        .setSmallIcon(R.mipmap.ic_launcher_round)
-                        //通知点开后的大标题
-                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                        //启动通知活动
-                        .setContentIntent(pi)
-                        //设置取消通知，方法一
-                        .setAutoCancel(true)
-                        //设置通知的重要程度
-                        .setPriority(NotificationCompat.PRIORITY_MAX)
-                        .setChannelId(chandalId)
-                        //通知是发出震动
-                        .setVibrate(new long[]{0, 1000, 1000, 1000})
-                        .build();
-                //notify让通知显示出来
-                manager.notify(SEND_NOTICE, notification);
+                    Intent intentNotice = new Intent(this, NotificationActivity.class);
+                    PendingIntent pi = PendingIntent.getActivity(this, 0, intentNotice, 0);
+
+                    //创建通知首先要创建一个NotificationManager来对通知进行管理，通过getSystemService获取到
+                    //里面需要穿一个字符串，一般传Context.NOTIFICATION_SERVICE
+                    NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    //接下来要用Builder来构造Notification对象，这里我们使用NotificationCompat类来构造创建Notification对象
+                    //确保我们的程序字啊所有android系统版本都能运行
+                    Notification notification = new NotificationCompat.Builder(this, "default")
+                            //通知的标题
+                            .setContentTitle("This is content Title")
+                            //通知的内容
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText("method int androidx.appcompat.widget." +
+                                    "DropDownListView.lookForSelectablePosition(int, boolean) \" +\n" +
+                                    "  \"would have incorrectly overridden the package-private " +
+                                    "method in android.widget.ListView<"))
+                            //显示大图
+                            // .setStyle(new NotificationCompat.BigPictureStyle().bigPicture
+                            //(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)))
+                            //发出通知的时间
+                            .setWhen(System.currentTimeMillis())
+                            //通知的小标题
+                            .setSmallIcon(R.mipmap.ic_launcher_round)
+                            //通知点开后的大标题
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                            //启动通知活动
+                            .setContentIntent(pi)
+                            //设置取消通知，方法一
+                            .setAutoCancel(true)
+                            //设置通知的重要程度
+                            .setPriority(NotificationCompat.PRIORITY_MAX)
+                            .setChannelId(chandalId)
+                            //通知是发出震动
+                            .setVibrate(new long[]{0, 1000, 1000, 1000})
+                            .build();
+                    //notify让通知显示出来
+                    manager.notify(SEND_NOTICE, notification);
+                } else {
+                    // Create a notification and set the notification channel.
+                    // 发布通知
+                    //实现通知的点击效果，使用PendingIntent来启动一个通知活动
+                    Intent intentNotice = new Intent(this, NotificationActivity.class);
+                    PendingIntent pi = PendingIntent.getActivity(this, 0, intentNotice, 0);
+
+                    //创建通知首先要创建一个NotificationManager来对通知进行管理，通过getSystemService获取到
+                    //里面需要穿一个字符串，一般传Context.NOTIFICATION_SERVICE
+                    NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    //接下来要用Builder来构造Notification对象，这里我们使用NotificationCompat类来构造创建Notification对象
+                    //确保我们的程序字啊所有android系统版本都能运行
+                    Notification notification = new NotificationCompat.Builder(this, "default")
+                            //通知的标题
+                            .setContentTitle("This is content Title")
+                            //通知的内容
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText("method int androidx.appcompat.widget." +
+                                    "DropDownListView.lookForSelectablePosition(int, boolean) \" +\n" +
+                                    "  \"would have incorrectly overridden the package-private " +
+                                    "method in android.widget.ListView<"))
+                            //显示大图
+                            // .setStyle(new NotificationCompat.BigPictureStyle().bigPicture
+                            //(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)))
+                            //发出通知的时间
+                            .setWhen(System.currentTimeMillis())
+                            //通知的小标题
+                            .setSmallIcon(R.mipmap.ic_launcher_round)
+                            //通知点开后的大标题
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                            //启动通知活动
+                            .setContentIntent(pi)
+                            //设置取消通知，方法一
+                            .setAutoCancel(true)
+                            //设置通知的重要程度
+                            .setPriority(NotificationCompat.PRIORITY_MAX)
+                            //通知是发出震动
+                            .setVibrate(new long[]{0, 1000, 1000, 1000})
+                            .build();
+                    //notify让通知显示出来
+                    manager.notify(SEND_NOTICE, notification);
+                }
+
+
                 break;
             default:
                 break;
